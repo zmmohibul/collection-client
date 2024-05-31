d4a9831d9c1f1b16b29c6c02a696e0c0f4b1a02d
zmmohibul <18-36427-1@student.aiub.edu>
2024-05-31 15:08:19 +0600

    Api Url Change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000/api',
-  // apiUrl: 'https://collection-a6x5.onrender.com/api',
+  // apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://collection-a6x5.onrender.com/api',
 };
